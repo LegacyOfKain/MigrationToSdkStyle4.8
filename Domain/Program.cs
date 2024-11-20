ffc09599e17c6be0783e2f5d792d1cbe46d6bc58
Priyanku <priyankubiswas85@gmail.com>
2024-11-20 23:27:16 +0100

    xml style csproj files

---
+++ Domain/Program.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchitecture.Common.Dates;
+using CleanArchitecture.Domain.Products;
+using CleanArchitecture.Domain.Sales;
+using System;
+
+namespace CleanArchitecture.Domain
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("jkjkj");
+
+
+
+            var _sale = new Sale();
+
+            _sale.UnitPrice = 89;
+            _sale.Product = new Product();
+            _sale.Product.Name = new DateService().GetDate().ToString();
+
+            Console.WriteLine(_sale.Product.Name);
+
+            Console.ReadKey();
+        }
+    }
+}
